cea8df3f473d2c13d00b1250a07853c25b9e708e
Tram Nguyen <tramngoc1402@gmail.com>
2022-08-08 14:48:40 +0300

    Set up file base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Result from './Result';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>English Dictionary</h1>
+
+      <form>
+        <textarea name='' id='' cols='30' rows='4' placeholder='Enter text' />
+
+        <div className='voices-icons'>
+          <div className='select-voice'>
+            <select name='' id=''>
+              <option value=''>English</option>
+              <option value=''>English</option>
+              <option value=''>English</option>
+            </select>
+          </div>
+
+          <i class='fa-solid fa-volume-high' />
+        </div>
+      </form>
+
+      <Result />
     </div>
   );
 }
---
+++ src/Result.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Result = () => {
+  return (
+    <ul>
+      <li className='world'>
+        <h2>HELLO</h2>
+        <span>/həˈləʊ,hɛˈləʊ/</span>
+      </li>
+
+      <li className='contain'>
+        <h3>noun</h3>
+        <div className='details meaning'>
+          <h3>Meaning</h3>
+          <p>-"Hello" or an equivalent greeting.</p>
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+export default Result;
